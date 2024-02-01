a054f55ac2e331c767605f6b4e44c3c8083235f1
saananokela <smnokela@gmail.com>
2024-02-01 15:24:54 +0200

    index-nimi korjattu ja lisätty js-tiedostoon toiminnallisuus

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+document.querySelector('#dice').addEventListener('click',() => {
+    // console.log("toimii");
+
+    // arvotaan satunnaisluku väliltä 1-6
+    const randomNumber =  Math.floor(Math.random() * 6) + 1;
+
+    // console.log("randomNumber:" + randomNumber);
+
+    // muodostetaan kuvan tiedostonimi-polku
+    const imagePath = './img/' + randomNumber + '.png';
+
+    // console.log("imagePath:" + imagePath);
+
+    // päivitetään kuvan src-ominaisuus dice-img -elementille
+    document.querySelector('#dice > img').src = imagePath;
+});
+
+
